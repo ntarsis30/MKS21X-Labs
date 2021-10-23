ecb4267ae0c454730c0d39f85c69de6315613554
Nicholas Tarsis <ntarsis30@stuy.edu>
2021-10-23 13:14:25 -0400

    Added first method

--- 01-Object/Demo.java
+++ 01-Object/Demo.java
@@ -1,5 +1,8 @@
 public class Demo{
   /* more methods can go here*/
+  public static double distance(Point p1, Point p2){
+      return Math.sqrt(Math.pow(p1.getX()-p2.getX(),2)+Math.pow(p1.getY()-p2.getY(),2));
+  }
 
   public static void main(String[]args){
     Point P1 = new Point(1,1);
