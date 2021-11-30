3757f63b63c58abf4f3a36652c5318f9b49c4589
Nicholas Tarsis <ntarsis30@stuy.edu>
2021-11-29 22:30:15 -0500

    Added random test

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -1,4 +1,16 @@
+import java.util.Arrays;
+import java.lang.Math;
+
+
 public class BinarySearch{
+  public static int[] Test(int size) {
+    int[] nums = new int[size];
+    for (int i = 0; i < size; i++) {
+      nums[i] = (int)(Math.random()*18+3);
+    }
+    Arrays.sort(nums);
+    return nums;
+  }
   public static int binarySearch(int[]data,int target){
     int lo = 0;
     int hi = data.length-1;
@@ -29,6 +41,8 @@ public class BinarySearch{
     result = binarySearch(new int[]{0,1,2,3,4,5,6},6);
     System.out.println(result);//should be 6
     //Tip: You can use a loop to find if it works on 0,1,2,3,4,5,and 6 in this array!
-
+    int[] test = Test(100);
+    result = binarySearch(test,6);
+    System.out.println(result);
   }
 }
