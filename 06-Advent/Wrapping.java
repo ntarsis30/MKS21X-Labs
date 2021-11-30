32217c547b3cf6998a1866e5d3bada70716fcd9f
Nicholas Tarsis <ntarsis30@stuy.edu>
2021-11-29 21:59:09 -0500

    Finished part 2 of AOC 2015 P2

--- 06-Advent/Wrapping.java
+++ 06-Advent/Wrapping.java
@@ -27,9 +27,36 @@ public class Wrapping{
       }
       return ans;
     }
+    public static int part2(String fileName){
+        int ans = 0;
+      try {
+          File file = new File(fileName);
+          Scanner input = new Scanner(file);
+          while (input.hasNextLine()) {
+              String line = input.nextLine();
+              String[] nums = line.split("x");
+              int a = Integer.parseInt(nums[0]);
+              int b = Integer.parseInt(nums[1]);
+              int c = Integer.parseInt(nums[2]);
+              int big = Math.max(a,b);
+              big = Math.max(big,c);
+              ans += a*b*c + 2*(a+b+c)-2*big;
+          }
+          input.close();
+      } catch (FileNotFoundException ex) {
+          //File not found
+      }
+      return ans;
+    }
     public static void main(String[] args){
         String test = args[0];
-        System.out.println(part1(test));
+        int num = Integer.parseInt(args[1]);
+        if(num==1){
+          System.out.println(part1(test));
 
+        }
+        else if (num==2){
+          System.out.println(part2(test));
+        }
     }
 }
