d47942104cf00ac4631eb41e1d56a735f1988834
Nicholas Tarsis <ntarsis30@stuy.edu>
2021-12-20 23:35:02 -0500

    Compeled the NoNullArrayList portion, the first of the ArrayList subclasses

---
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  private T value;
+  public NoNullArrayList() {
+    super();
+  }
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+  public T set(int idx, T val) {
+    if (val == null) {
+      throw new IllegalArgumentException();
+    }
+    super.set(idx, val);
+    return val;
+  }
+  public boolean add(T val) {
+    if (val == null) {
+      throw new IllegalArgumentException();
+    }
+    super.add(val);
+    return true;
+  }
+  public void add(int idx, T val) {
+    if (val == null) {
+      throw new IllegalArgumentException();
+    }
+    super.add(idx, val);
+  }
+}
