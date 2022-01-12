cd015af7bec1307fb2c9ac0a09156c159ecb90bd
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-01-11 22:50:51 -0500

    Finished adding the array with random integers and displaying them

--- 12-Stuyablo/Screen.java
+++ 12-Stuyablo/Screen.java
@@ -1,24 +1,43 @@
+import java.util.Random;
 public class Screen{
   public static void main(String[] args){
+    
     for (int i = 1; i <= 30;i++){
       Text.go(i,1);
-      System.out.print(Text.colorize(" ", Text.RED+Text.BACKGROUND));
+      System.out.print(Text.colorize(" ", Text.MAGENTA+Text.BACKGROUND));
     }
-    Text.go(30,1);
-    for (int i = 0; i < 80;i++){
-      Text.go(30,i+1);
-      System.out.print(Text.colorize(" ", Text.RED+Text.BACKGROUND));
+    for (int i = 1; i <= 80;i++){
+      Text.go(30,i);
+      System.out.print(Text.colorize(" ", Text.MAGENTA+Text.BACKGROUND));
     }
-    Text.go(30,80);
-    for (int i = 0; i < 30;i++){
-      Text.go(i+1,80);
-      System.out.print(Text.colorize(" ", Text.RED+Text.BACKGROUND));
+    for (int i = 1; i <= 30;i++){
+      Text.go(i,80);
+      System.out.print(Text.colorize(" ", Text.MAGENTA+Text.BACKGROUND));
     }
-    Text.go(1,80);
-    for (int i = 0; i < 80;i++){
-      Text.go(1,i+1);
-      System.out.print(Text.colorize(" ", Text.RED+Text.BACKGROUND));
+    for (int i = 1; i <= 80;i++){
+      Text.go(1,i);
+      System.out.print(Text.colorize(" ", Text.MAGENTA+Text.BACKGROUND));
     }
+    int[] nums = new int[4];
+    Text.go(2,2);
+    for (int i = 0; i < 4; i++){
+      Random rand = new Random();
+      nums[i]= rand.nextInt(100);
+      int color = Text.WHITE;
+      if(nums[i]<25){
+        color = Text.RED;
+      }
+      else if (nums[i]>75){
+        color = Text.GREEN;
+      }
+      String display = Integer.toString(color);  
+      System.out.print(Text.colorize(display, Text.MAGENTA));
+      Text.go(2,2+(i+1)*6);
+
+
+    }
+    Text.go(31,1);
+    
 
   }
 
